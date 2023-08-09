0a65ef444cc04af6344a40fd92207e3dc0ec7e82
M1058896 <M1058896@A2ML37876.mindtree.com>
2023-08-09 15:59:46 +0530

    TestNG

--- src/test/java/testCasesSuite/FIrstTestCase.java
+++ src/test/java/testCasesSuite/FIrstTestCase.java
@@ -19,32 +19,32 @@ import utility.TestBaseWeb;
 public class FIrstTestCase extends TestBaseWeb {
 	loginPage lp;
 
-	@BeforeClass
+	@BeforeClass(groups = {"p1"})
 	void AfterClass() {
 		System.out.println("---------------------------------------------");
 		System.out.println("Starting the execution");
 	}
 
-	@AfterClass
+	@AfterClass(groups = {"p1"})
 	void BeofreClass() {
 		System.out.println("---------------------------------------------");
 		System.out.println("Ending the execution");
 	}
 
-	@BeforeMethod
+	@BeforeMethod(groups = {"p1"})
 	void BrowserSetUp() {
 		System.out.println("Will set up Browser here");
 		intilize();
 
 	}
 
-	@AfterMethod
+	@AfterMethod(groups = {"p1"})
 	void BrowserTearUp() {
 		System.out.println("Will close teh broser Browser here");
 		driver.close();
 	}
 
-	@Test(alwaysRun=true)
+	@Test(groups=("sanity"))
 	void Login() {
 		lp = new loginPage(driver);
 		
@@ -55,8 +55,33 @@ public class FIrstTestCase extends TestBaseWeb {
 		lp.ClickOnLoginButton("I click on login link");
 		lp.VerifyPageTitle("I verify page title was as expeted");
 	}
-	@Test(alwaysRun=true)
+	@Test(groups=("regression"))
 	void HomePageValidation() {
+		lp = new loginPage(driver);
+		lp.navigateTourl(driver, "https://www.nopcommerce.com/en/", "Launch the URL");
+		lp.clickOnLoginLink("Click on login Link");
+		System.out.println("Entering Username");
+		lp.setUserName("pramod3474@gmail.com", "I enter Valid User Name");
+		lp.setPassword("Pramod@3474", "I enter Valid Password");
+		lp.ClickOnLoginButton("I click on login link");
+		lp.VerifyPageTitle("I verify page title was as expeted");
+		lp.clickOnFeatures("I click on Features under Product");
+		lp.VerifyFeaturePage("I Verify Feature Page");
+		
+	}
+	@Test(groups=("regression"))
+	void Login1() {
+		lp = new loginPage(driver);
+		
+		lp.navigateTourl(driver, "https://www.nopcommerce.com/en/", "Launch the URL");
+		lp.clickOnLoginLink("Click on login Link");
+		lp.setUserName("pramod3474@gmail.com", "I enter Valid User Name");
+		lp.setPassword("Pramod@3474", "I enter Valid Password");
+		lp.ClickOnLoginButton("I click on login link");
+		lp.VerifyPageTitle("I verify page title was as expeted");
+	}
+	@Test(groups=("sanity"))
+	void HomePageValidation1() {
 		lp = new loginPage(driver);
 		
 		lp.navigateTourl(driver, "https://www.nopcommerce.com/en/", "Launch the URL");
